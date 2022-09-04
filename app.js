2e2fe99ab62cde8a0c4817db4c6bb06f69973f68
muzammilawan850 <109416838+muzammilawan850@users.noreply.github.com>
2022-09-04 15:16:42 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,62 @@
+const { response } = require('express');
+const express = require('express');
+ 
+// Creating express object
+const app = express();
+// Parse JSON bodies (as sent by API clients)
+app.use(express.json());
+
+//init mail api
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'ciberteste50@gmail.com',
+    pass: 'tbnescsctrdbsehv'
+  }
+});
+
+
+ 
+// Handling GET request
+app.get('/', (req, res) => {
+    res.send('A simple Node App is '
+        + 'running on this server')
+    res.end()
+})
+
+
+
+
+
+app.get('/sendMail', (req, res) => {
+  const {from,messageBody } = req.query;
+  console.log(req.query);
+
+    var mailOptions = {
+        from: 'ciberteste50@gmail.com',
+        to: 'ciberteste50@gmail.com',
+        subject: from,
+        text: messageBody
+      };
+      
+      transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+          res.send(400);
+        } else {
+          console.log('Email sent: ' + info.response);
+          res.send(200);
+        }
+      });
+   
+})
+ 
+// Port Number
+const PORT = process.env.PORT || 5000;
+ 
+// Server Setup
+app.listen(3000, "192.168.0.105", ()=>{
+    console.log("server started and running on port " + 3000);
+});
